943e9ad4fa0dcc24b31ebb69bce135e353d6e374
thehanemperor <sh442555789@gmail.com>
2025-03-01 21:11:21 -0800

    File updated at 3/1/2025, 9:11:22 PM

--- 80-RemoveDuplicatesfromSortedArrayII/80-RemoveDuplicatesfromSortedArrayII.py
+++ 80-RemoveDuplicatesfromSortedArrayII/80-RemoveDuplicatesfromSortedArrayII.py
@@ -1,5 +1,6 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
+        return self.followUp(nums)
         seen = {}
         for n in nums:
             seen[n] = seen.get(n,0) + 1
@@ -10,3 +11,18 @@ class Solution:
                 maxVal = v
                 maxKey = k
         return maxKey
+
+    def followUp(self,nums):
+        # because the most frequent is over a half
+        # we can store only one candidate count
+        count = 0
+        candidate = None
+        for num in nums:
+            if count == 0:
+                candidate = num
+            if num != candidate:
+                count -=1
+            else:
+                count += 1
+
+        return candidate
