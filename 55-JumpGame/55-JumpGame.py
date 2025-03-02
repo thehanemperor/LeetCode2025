293147c080870edb6d55c4eda069a5ce7b827f20
thehanemperor <sh442555789@gmail.com>
2025-03-01 23:17:18 -0800

    Create file

---
+++ 55-JumpGame/55-JumpGame.py
@@ -0,0 +1,13 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        n = len(nums)
+        dp = [False] * n
+        dp[n-1] = True
+        for i in range(n-2,-1,-1):
+            for j in range(nums[i]+1):
+                
+                if dp[i+j] == True:
+                    dp[i] = True
+                    break
+        
+        return dp[0]
