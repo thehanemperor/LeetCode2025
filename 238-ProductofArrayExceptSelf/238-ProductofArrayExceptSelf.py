1b1b29dd187fcc56cb064aebce6d4fe9d43d5471
thehanemperor <sh442555789@gmail.com>
2025-03-02 14:30:33 -0800

    File updated at 3/2/2025, 2:30:33 PM

--- 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.py
+++ 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.py
@@ -1,6 +1,5 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        return self.followUp(nums)
         n = len(nums)
         left = [1] * n
         right = [1] * n
