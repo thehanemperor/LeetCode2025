9258b4fc312fab77f276a8dfb6ac46f5345cc544
thehanemperor <sh442555789@gmail.com>
2025-03-01 20:40:04 -0800

    File updated at 3/1/2025, 8:40:04 PM

--- 26-RemoveDuplicatesfromSortedArray/26-RemoveDuplicatesfromSortedArray.py
+++ 26-RemoveDuplicatesfromSortedArray/26-RemoveDuplicatesfromSortedArray.py
@@ -1,11 +1,10 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        left = 0
+        left = 1
         seen = set()
-        for i in range(len(nums)):
-            if nums[i] not in seen:
+        for i in range(1,len(nums)):
+            if nums[i-1] != nums[i]:
                 nums[left] = nums[i]
                 left += 1
-            seen.add(nums[i])
 
         return left
