472af89afd9b4177131f781ce6f49d8b5a637d33
thehanemperor <sh442555789@gmail.com>
2025-03-03 14:58:28 -0800

    File updated at 3/3/2025, 2:58:28 PM

--- 76-MinimumWindowSubstring/76-MinimumWindowSubstring.py
+++ 76-MinimumWindowSubstring/76-MinimumWindowSubstring.py
@@ -9,14 +9,11 @@ class Solution:
             curr = s[right]
             seen[curr] = seen.get(curr,0) + 1
             if curr in check and seen[curr]==check[curr]:
-                
                 formed += 1
             
             while left <=right and formed == len(check):
-                if formed == len(check):
-                    
-                    if right - left + 1 < res[0]:
-                        res = [right-left+1,left,right]
+                if right - left + 1 < res[0]:
+                    res = [right-left+1,left,right]
                 seen[s[left]] -= 1
                 if s[left] in check and seen[s[left]] < check[s[left]]:
                     formed -= 1
@@ -24,5 +21,5 @@ class Solution:
                 left += 1
 
             right += 1
-        print(res)
+
         return "" if res[0] == float("inf") else s[res[1]:res[2]+1]
