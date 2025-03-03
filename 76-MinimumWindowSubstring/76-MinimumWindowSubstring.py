9dfb117f536684719b690698176d1d6d06da5e9e
thehanemperor <sh442555789@gmail.com>
2025-03-03 14:57:46 -0800

    Create file

---
+++ 76-MinimumWindowSubstring/76-MinimumWindowSubstring.py
@@ -0,0 +1,28 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        check = Counter(t)
+        left = right = 0
+        seen = {}
+        formed = 0
+        res = [float("inf"),-1,-1]
+        while right < len(s):
+            curr = s[right]
+            seen[curr] = seen.get(curr,0) + 1
+            if curr in check and seen[curr]==check[curr]:
+                
+                formed += 1
+            
+            while left <=right and formed == len(check):
+                if formed == len(check):
+                    
+                    if right - left + 1 < res[0]:
+                        res = [right-left+1,left,right]
+                seen[s[left]] -= 1
+                if s[left] in check and seen[s[left]] < check[s[left]]:
+                    formed -= 1
+                
+                left += 1
+
+            right += 1
+        print(res)
+        return "" if res[0] == float("inf") else s[res[1]:res[2]+1]
