811b8e8daef4f11d0c9dc3f454ec6df843e6ceba
thehanemperor <sh442555789@gmail.com>
2025-03-02 23:47:09 -0800

    File updated at 3/2/2025, 11:47:10 PM

--- 125-ValidPalindrome/125-ValidPalindrome.py
+++ 125-ValidPalindrome/125-ValidPalindrome.py
@@ -1,5 +1,6 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+        return self.followUp(s)
         arr = []
         for c in s:
             if c.isdigit() or c.isalpha():
@@ -9,6 +10,22 @@ class Solution:
         while left < right:
             if arr[left] != arr[right]:
                 return False
+            left += 1
+            right -= 1
+
+        return True
+
+    def followUp(self,s):
+        left,right = 0, len(s) -1
+        while left < right:
+            while left < right and not s[left].isdigit() and not s[left].isalpha():
+                left += 1
+            while left < right and not s[right].isdigit() and not s[right].isalpha():
+                right -= 1
+            
+            if s[left].lower() != s[right].lower():
+                return False
+            
             left += 1
             right -= 1
 
