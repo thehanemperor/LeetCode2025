2e5f1d2d45662df638eb5d827e66fd5edde70d5f
thehanemperor <sh442555789@gmail.com>
2025-03-03 23:50:34 -0800

    Create file

---
+++ 289-GameofLife/289-GameofLife.py
@@ -0,0 +1,28 @@
+class Solution:
+    def gameOfLife(self, board: List[List[int]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        m = len(board)
+        n = len(board[0])
+        copy = [[board[i][j] for j in range(n)] for i in range(m)]
+        neighbor= [(0,1),(1,0),(-1,0),(0,-1),(1,1),(1,-1),(-1,1),(-1,-1)]
+        for i in range(m):
+            for j in range(n):
+                live = 0
+                for x,y in neighbor:
+                    if 0<=i+x<m and 0<=j+y<n and board[i+x][j+y] == 1:
+                        live += 1
+
+                if live < 2:
+                    copy[i][j] = 0
+
+                if live >3:
+                    copy[i][j] = 0
+
+                if board[i][j] == 0 and live == 3:
+                    copy[i][j] = 1
+
+        for i in range(m):
+            for j in range(n):
+                board[i][j] = copy[i][j]
