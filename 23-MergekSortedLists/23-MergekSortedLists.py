6c27b907fcd877a56fc2f929d752c3acff5c3e43
thehanemperor <sh442555789@gmail.com>
2025-03-06 01:52:50 -0800

    File updated at 3/6/2025, 1:52:51 AM

--- 23-MergekSortedLists/23-MergekSortedLists.py
+++ 23-MergekSortedLists/23-MergekSortedLists.py
@@ -5,34 +5,20 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        # if not lists or not lists[0]:
-        #     return None
-        k = len(lists)
-        merging = True
         dummy = ListNode()
         root = dummy
-        index = 0
-        while merging:
-            end = 0
-            nodeI = None
-            minNum = 999999
-            for i in range(k):
-                # print(i, lists[i])
-                if not lists[i]:
-                    end += 1
-                    continue
-                if lists[i].val < minNum:
-                    minNum = lists[i].val
-                    nodeI = i
-            
-            
-               
-            if end == k:
-                merging = False
-            else:
-                curr = ListNode(minNum)
-                root.next = curr
-                root = root.next
-                lists[nodeI] = lists[nodeI].next
+        heap = []
+
+        for node in lists:
+            if node:
+                heapq.heappush(heap,(node.val,{node}))
+
+        while heap:
+            val,node = heapq.heappop(heap)
+            root.next = ListNode(val)
+            root = root.next
+            node = node.pop()
+            if node.next:
+                heapq.heappush(heap,(node.next.val, {node.next}))
         
         return dummy.next
