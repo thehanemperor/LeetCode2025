42a5741768a4f40eb78298b4357802fb9c9a7412
thehanemperor <sh442555789@gmail.com>
2025-03-08 00:33:21 -0800

    File updated at 3/8/2025, 12:33:22 AM

--- 539-MinimumTimeDifference/539-MinimumTimeDifference.py
+++ 539-MinimumTimeDifference/539-MinimumTimeDifference.py
@@ -1,5 +1,6 @@
 class Solution:
     def findMinDifference(self, timePoints: List[str]) -> int:
+        return self.followUp(timePoints)
         arr = []
         for time in timePoints:
             hr,m = time.split(":")
@@ -11,5 +12,32 @@ class Solution:
             res = min(res, arr[i]- arr[i-1])
         
         res = min(24*60 - arr[-1] + arr[0], res)
+
+        return res
+
+    def followUp(self, timePoints):
+        clock = [False] * (24*60)
+        for time in timePoints:
+            hr, m = time.split(":")
+            curr = int(hr)*60+int(m)
+            if clock[curr]:
+                return 0
+            clock[curr] = True
+
+        first = None
+        prev = None
+        curr = None
+        res = float("inf")
+        for i in range(24*60):
+            if clock[i]:
+                if first == None:
+                    first = i
+                curr = i
+
+                if prev:
+                    res = min(res, curr - prev)
+
+                prev = curr
+        res = min(res, 24*60 - curr + first)
 
         return res
