7dfb0a7afd5713bdfd051be6be3eaee6ea728c65
thehanemperor <sh442555789@gmail.com>
2025-03-08 02:00:51 -0800

    File updated at 3/8/2025, 2:00:53 AM

--- 41-FirstMissingPositive/41-FirstMissingPositive.py
+++ 41-FirstMissingPositive/41-FirstMissingPositive.py
@@ -1,5 +1,6 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
+        return self.followUp(nums)
         n = len(nums)
         seen = [False] * (n+1)
 
@@ -10,5 +11,24 @@ class Solution:
         for i in range(1,n+1):
             if not seen[i]:
                 return i
+
+        return n+1
+
+    def followUp(self,nums): # O(1) Space
+        n = len(nums)
+        i = 0
+        while i < n:
+            indexGoTo = nums[i]-1
+            while 0<nums[i]<=n and nums[i] != nums[indexGoTo]:
+                nums[i], nums[indexGoTo] = nums[indexGoTo], nums[i]
+                indexGoTo = nums[i]-1
+            
+            i += 1    
+
+        for i in range(n):
+            if nums[i] != i+1:
+                return i+1
+
+        return n+1
 
         return n+1
