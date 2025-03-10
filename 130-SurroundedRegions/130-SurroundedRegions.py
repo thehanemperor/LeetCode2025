a6483beae3813c9db8d79b7d74ff8e337b047743
thehanemperor <sh442555789@gmail.com>
2025-03-09 20:21:13 -0700

    File updated at 3/9/2025, 8:21:14 PM

--- 130-SurroundedRegions/130-SurroundedRegions.py
+++ 130-SurroundedRegions/130-SurroundedRegions.py
@@ -3,33 +3,34 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        
         m = len(board)
         n = len(board[0])
+        edges = []
         for i in range(m):
             for j in range(n):
-                if (i == 0 or j == 0 or i ==m-1 or j == n-1) and board[i][j] == "O":
-                    queue = deque([(i,j)])
-                    self.bfs(queue,"E",board)
-                
+                if (i in [0,m-1] or j in [0,n-1]) and board[i][j] == "O":
+                    edges.append((i,j))
+
+        self.bfs(deque(edges),board,"E")
+
         for i in range(m):
             for j in range(n):
                 if board[i][j] == "O":
-                    queue = deque([(i,j)])
-                    self.bfs(queue,"X",board)
+                    self.bfs(deque([(i,j)]), board, "X")
 
         for i in range(m):
             for j in range(n):
                 if board[i][j] == "E":
                     board[i][j] = "O"
 
-    def bfs(self,queue,mark,board):
+        
+    def bfs(self,queue,board,mark):
         m = len(board)
         n = len(board[0])
         while queue:
             x,y = queue.popleft()
             board[x][y] = mark
-            for i,j in [(1,0),(0,1),(-1,0),(0,-1)]:
-                if 0<= x+i <m and 0<=y+j <n and board[x+i][y+j] == "O":
-                    board[x+i][y+j] = mark
-                    queue.append((x+i,j+y)) 
+            for dx,dy in [(1,0),(0,1),(-1,0),(0,-1)]:
+                if 0<=x+dx<m and 0<=y+dy<n and board[dx+x][dy+y] == "O":
+                    queue.append((dx+x,dy+y))
+                    board[dx+x][y+dy] = mark
