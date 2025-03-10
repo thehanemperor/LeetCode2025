4f35a46246de93874aed3a5fc5c0c5c376f6123e
thehanemperor <sh442555789@gmail.com>
2025-03-09 22:27:49 -0700

    File updated at 3/9/2025, 10:27:50 PM

--- 42-TrappingRainWater/42-TrappingRainWater.py
+++ 42-TrappingRainWater/42-TrappingRainWater.py
@@ -1,21 +1,18 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        i = 0
         stack = []
-        n = len(height)
         res = 0
-        while i <n:
-            while stack and height[i] > height[stack[-1]]:
+        for i in range(len(height)):
+            while stack and height[stack[-1]] < height[i]:
                 mid = stack.pop()
                 if not stack:
-                    continue # no left boundary
-
-                distance = i - stack[-1] -1
-                boundHeight = min(height[stack[-1]], height[i]) - height[mid]
-                area = distance * boundHeight
+                    break
+                
+                boundHeight = min(height[i], height[stack[-1]]) - height[mid]
+                length = i - stack[-1] -1
+                area = boundHeight * length
                 res += area
 
             stack.append(i)
-            i += 1
 
         return res
