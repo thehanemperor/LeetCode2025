737387903ded712f7ac3ac285d0d4c6dae487cd0
thehanemperor <sh442555789@gmail.com>
2025-03-13 00:49:17 -0700

    File updated at 3/13/2025, 12:49:18 AM

--- 1790-CheckifOneStringSwapCanMakeStringsEqual/1790-CheckifOneStringSwapCanMakeStringsEqual.py
+++ 1790-CheckifOneStringSwapCanMakeStringsEqual/1790-CheckifOneStringSwapCanMakeStringsEqual.py
@@ -1,13 +1,16 @@
 class Solution:
     def areAlmostEqual(self, s1: str, s2: str) -> bool:
-        seen1 = Counter(s1)
-        seen2 = Counter(s2)
-        if seen1 != seen2:
-            return False
+        if s1 == s2:
+            return True
+        count1 = Counter(s1)
+        count2 = Counter(s2)
 
-        wrong = 0
+        if count1 != count2:
+            return False
+        
+        swap = 0
         for i in range(len(s1)):
             if s1[i] != s2[i]:
-                wrong += 1
+                swap += 1
 
-        return wrong <=2
+        return swap==2
