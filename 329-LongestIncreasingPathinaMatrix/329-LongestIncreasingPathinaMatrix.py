735ee1013aab288d575b26bc192823c80032e6a6
thehanemperor <sh442555789@gmail.com>
2025-03-15 15:23:40 -0700

    File updated at 3/15/2025, 3:23:41 PM

--- 329-LongestIncreasingPathinaMatrix/329-LongestIncreasingPathinaMatrix.py
+++ 329-LongestIncreasingPathinaMatrix/329-LongestIncreasingPathinaMatrix.py
@@ -2,27 +2,25 @@ class Solution:
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
         m = len(matrix)
         n = len(matrix[0])
-        res = 0
         seen = {}
+        res = 0
         for i in range(m):
             for j in range(n):
-                curr = self.dfs(matrix,i,j,seen)
-                res = max(res,curr)
-        
+                seen[(i,j)] = self.dfs(matrix,i,j,seen)
+                res = max(res,seen[(i,j)])
+
         return res
 
-    def dfs(self,matrix,x,y,seen):
-        if (x,y) in seen:
-            return seen[(x,y)]
-        directions = [(0,1),(1,0),(0,-1),(-1,0)]
+    def dfs(self,matrix,i,j,seen):
+        if (i,j) in seen:
+            return seen[(i,j)]
+
         m = len(matrix)
         n = len(matrix[0])
-        step = 0
-        for dx,dy in directions:
-            count = 0
-            if 0<= x+dx<m and 0<=y+dy<n and matrix[dx+x][dy+y] > matrix[x][y]:
-                count = self.dfs(matrix,dx+x,dy+y,seen)
-            step = max(step, count)
-
-        seen[(x,y)] = step+1
-        return step+1
+        directions = [(0,1),(1,0),(-1,0),(0,-1)]
+        res = 1
+        for x,y in directions:
+            if 0<=i+x<m and 0<=j+y<n and matrix[i+x][j+y] > matrix[i][j]:
+                res = max(res,self.dfs(matrix,i+x,j+y,seen)+1)
+        seen[(i,j)] = res
+        return res
