4842b8fb2341eb3a6a0bfa19a9f333fe11ac3011
thehanemperor <sh442555789@gmail.com>
2025-03-15 00:15:22 -0700

    Create file

---
+++ 277-FindtheCelebrity/277-FindtheCelebrity.py
@@ -0,0 +1,20 @@
+# The knows API is already defined for you.
+# return a bool, whether a knows b
+# def knows(a: int, b: int) -> bool:
+
+class Solution:
+    def findCelebrity(self, n: int) -> int:
+        candidate = 0
+        for i in range(1,n):
+            if knows(candidate,i):
+                candidate = i
+
+        for i in range(n):
+            if i == candidate:
+                continue
+            # candidate should not know i
+            # i must know candidate
+            if knows(candidate,i) or not knows(i,candidate):
+                return -1
+
+        return candidate
