9a905a414547b9f7dd953bd7e22de9f6f6297729
thehanemperor <sh442555789@gmail.com>
2025-03-15 17:17:59 -0700

    File updated at 3/15/2025, 5:18:00 PM

--- 124-BinaryTreeMaximumPathSum/124-BinaryTreeMaximumPathSum.py
+++ 124-BinaryTreeMaximumPathSum/124-BinaryTreeMaximumPathSum.py
@@ -5,17 +5,56 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def __init__(self):
-        self.pathsum=float("-inf")
-    def maxPathSum(self, root: Optional[TreeNode]) -> int:
-        self.helper(root)
-        return self.pathsum
-    def helper(self,root):
+    def maxPathSum(self, root: TreeNode) -> int:
+        self.res = -float("inf")
+        self.resPath = None
+        self.followUp(root)
+        return sum(self.resPath)
+
+    def dfs(self,root):
         if not root:
             return 0
-        left = max(0, self.helper(root.left))
-        right = max(0, self.helper(root.right))
-        self.pathsum=max(self.pathsum,left+right+root.val)
-        return max(left,right)+root.val
 
-        
+        left = max(0,self.dfs(root.left))
+        right = max(0, self.dfs(root.right))
+        curr = root.val + left + right
+        self.res = max(self.res, curr)
+
+        return max(root.val + left, root.val+right)
+
+    def followUp(self,root):
+        if not root:
+            return 0,[]
+
+        leftMax,leftPath = self.followUp(root.left)
+        rightMax,rightPath = self.followUp(root.right)
+
+        if leftMax<0:
+            leftMax = 0
+            leftPath = []
+
+        if rightMax<0:
+            rightMax = 0
+            rightPath = []
+
+        currentMax = root.val + leftMax + rightMax
+        if leftMax >0 and rightMax>0:
+            currentPath = leftPath+ [root.val]+ rightPath
+        elif leftMax >0:
+            currentPath = leftPath + [root.val]
+        elif rightMax >0:
+            currentPath = [root.val] + rightPath
+        else:
+            currentPath = [root.val]
+
+        if currentMax > self.res:
+            self.res = currentMax
+            self.resPath = currentPath
+
+        if leftMax > rightMax:
+            return root.val + leftMax, leftPath +[root.val]
+        else:
+            return root.val + rightMax, [root.val] + rightPath
+
+
+
