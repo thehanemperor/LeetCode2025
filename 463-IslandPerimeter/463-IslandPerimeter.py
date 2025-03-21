8ca4898528c331029a68500307b506559678b97e
thehanemperor <sh442555789@gmail.com>
2025-03-21 01:23:19 -0700

    File updated at 3/21/2025, 1:23:20 AM

--- 463-IslandPerimeter/463-IslandPerimeter.py
+++ 463-IslandPerimeter/463-IslandPerimeter.py
@@ -1,33 +1,19 @@
-// Last updated: 3/21/2025, 1:17:24 AM
+// Last updated: 3/21/2025, 1:23:20 AM
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
         m = len(grid)
         n = len(grid[0])
-        edge = [[0 for _ in range(n)] for _ in range(m)]
-        seen = set()
-        queue = deque()
+
+        res = 0
+
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == 1:
-                    queue.append((i,j))
-                    break
-            if queue:
-                break
-        
-        while queue:
-            
-            x,y = queue.popleft()
-            seen.add((x,y))
+                    up= 0 if i == 0 else grid[i-1][j]
+                    left = 0 if j == 0 else grid[i][j-1]
+                    right = 0 if j == n-1 else grid[i][j+1]
+                    bot = 0 if i == m-1 else grid[i+1][j]
 
-            for dx,dy in [(0,1),(1,0),(-1,0),(0,-1)]:
-                if 0<= x+dx <m and 0<=y+dy<n and grid[x+dx][y+dy] == 1:
-                    edge[x][y] += 1
-                    if (x+dx,y+dy) not in seen:
-                        seen.add((x+dx,y+dy))
-                        queue.append((x+dx,y+dy))
-
-        res = 0
-        for i,j in seen:
-            res += 4 - edge[i][j]
+                    res += 4 -(up+left+right+bot)
 
         return res
