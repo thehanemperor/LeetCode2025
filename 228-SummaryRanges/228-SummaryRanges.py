5d2a67bd27fa1e364bb455619b636224b2dfa816
thehanemperor <sh442555789@gmail.com>
2025-03-21 01:32:25 -0700

    File updated at 3/21/2025, 1:32:26 AM

--- 228-SummaryRanges/228-SummaryRanges.py
+++ 228-SummaryRanges/228-SummaryRanges.py
@@ -1,16 +1,17 @@
+// Last updated: 3/21/2025, 1:32:26 AM
 class Solution:
     def summaryRanges(self, nums: List[int]) -> List[str]:
         left = right = 0
         n = len(nums)
         res = []
         while right < n:
-            while right +1 <n and nums[right+1] - nums[right] == 1:
+            while right+1 <n and nums[right]+1 == nums[right+1]:
                 right += 1
-            if left == right:
-                res.append(str(nums[right]))
+
+            if left < right:
+                res.append("{}->{}".format(nums[left],nums[right]))
             else:
-                res.append("{}->{}".format(str(nums[left]), str(nums[right])))
-            
+                res.append(str(nums[right]))
             right += 1
             left = right
 
