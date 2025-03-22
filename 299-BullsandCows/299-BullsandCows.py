aca91fccc10f3be66305225abfe2612ff9aa3576
thehanemperor <sh442555789@gmail.com>
2025-03-22 02:03:31 -0700

    Create file

---
+++ 299-BullsandCows/299-BullsandCows.py
@@ -0,0 +1,22 @@
+// Last updated: 3/22/2025, 2:03:32 AM
+class Solution:
+    def getHint(self, secret: str, guess: str) -> str:
+        seen = {}
+        for i in range(len(secret)):
+            seen[secret[i]] = seen.get(secret[i],0) + 1
+
+        bull = 0
+        cow = 0
+        for i in range(len(guess)):
+            if guess[i] == secret[i]:
+                bull += 1
+                seen[secret[i]] -= 1
+
+        print(seen)
+        for i in range(len(guess)):
+
+            if guess[i]!=secret[i] and guess[i] in seen and seen[guess[i]] >0:
+                cow += 1
+                seen[guess[i]] -= 1
+
+        return "{}A{}B".format(bull,cow)
