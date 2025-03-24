210307b558c1704e169836b711cdfc423d9ef396
thehanemperor <sh442555789@gmail.com>
2025-03-24 02:03:44 -0700

    File updated at 3/24/2025, 2:03:45 AM

--- 744-FindSmallestLetterGreaterThanTarget/744-FindSmallestLetterGreaterThanTarget.py
+++ 744-FindSmallestLetterGreaterThanTarget/744-FindSmallestLetterGreaterThanTarget.py
@@ -1,15 +1,16 @@
-# Last updated: 3/24/2025, 1:55:15 AM
+# Last updated: 3/24/2025, 2:03:45 AM
 class Solution:
-    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+    def searchInsert(self, nums: List[int], target: int) -> int:
         left = 0
-        right = len(letters) -1
+        right = len(nums) -1
+
         while left <= right:
-            mid = left + (right- left) //2
-            if ord(letters[mid]) <= ord(target):
+            mid = left + (right-left)//2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
                 left = mid + 1
             else:
                 right = mid -1
 
-        if left >= len(letters):
-            return letters[0]
-        return letters[left]
+        return left
