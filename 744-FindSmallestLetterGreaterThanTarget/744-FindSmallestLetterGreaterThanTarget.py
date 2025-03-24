097b5abc7ccb0f788cb910bb228d0d2e3d260aed
thehanemperor <sh442555789@gmail.com>
2025-03-24 02:32:42 -0700

    File updated at 3/24/2025, 2:32:43 AM

--- 744-FindSmallestLetterGreaterThanTarget/744-FindSmallestLetterGreaterThanTarget.py
+++ 744-FindSmallestLetterGreaterThanTarget/744-FindSmallestLetterGreaterThanTarget.py
@@ -1,32 +1,18 @@
-# Last updated: 3/24/2025, 2:16:59 AM
+# Last updated: 3/24/2025, 2:32:43 AM
 class Solution:
-    def searchRange(self, nums: List[int], target: int) -> List[int]:
+    def findMin(self, nums: List[int]) -> int:
         n = len(nums)
         left = 0
-        right = n-1
-        res = []
-
-        while left <= right:
-            mid = left + (right-left)//2
-            if nums[mid] >= target:
-                right = mid -1
-
-            else:
-                left = mid + 1
-
-        if left == n or nums[left]!= target:
-            return [-1,-1]
-        
-        res.append(left)
-
-        left = 0
         right = n -1
+        if nums[left] <= nums[right]:
+            return nums[left]
+
         while left <= right:
-            mid = left + (right-left)//2
-            if nums[mid] <= target:
+            mid = left + (right-left) //2
+            if nums[mid] > nums[n-1]:
                 left = mid + 1
             else:
-                right = mid -1
+                right = mid - 1
 
-        res.append(right)
-        return res        
+        return nums[left]
+        
