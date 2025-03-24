fab6c88085639bbddd6be02212b1200cbf4af343
thehanemperor <sh442555789@gmail.com>
2025-03-24 02:58:47 -0700

    File updated at 3/24/2025, 2:58:48 AM

--- 33-SearchinRotatedSortedArray/33-SearchinRotatedSortedArray.py
+++ 33-SearchinRotatedSortedArray/33-SearchinRotatedSortedArray.py
@@ -1,32 +1,31 @@
+# Last updated: 3/24/2025, 2:58:48 AM
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-
+        n = len(nums)
         left = 0
-        right = len(nums) - 1
-
+        right = n -1
         while left <= right:
-            mid = (left + right) //2
-            if nums[mid] > nums[-1]:
-                left = mid + 1
-            else:
-                right = mid - 1
-        
-        res = self.binSearch(0, left -1, nums, target)
-        if res != -1:
-            return res
-        res = self.binSearch(left, len(nums)-1, nums, target)
-        return res
-
-    def binSearch(self, leftBound, rightBound, nums, target):
-        left = leftBound
-        right = rightBound
-        while left <= right:
-            mid = (left + right) //2
+            mid = left +(right-left)//2
             if nums[mid] == target:
                 return mid
-            elif target < nums[mid]:
-                right = mid -1
-            else:
+            if nums[mid] > nums[n-1]:
                 left = mid + 1
+            else:
+                right = mid -1
 
-        return -1  
+        if nums[left] <= target <= nums[n-1]:
+            right = n-1
+        else:
+            right = left
+            left = 0
+        print(left,right)
+        while left <= right:
+            mid = left + (right-left)//2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid +1
+            else:
+                right = mid -1
+
+        return -1
