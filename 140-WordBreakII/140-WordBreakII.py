a9194024cd71f810f689a310363f9952a0151b9f
thehanemperor <sh442555789@gmail.com>
2025-03-26 23:32:31 -0700

    File updated at 3/26/2025, 11:32:32 PM

--- 140-WordBreakII/140-WordBreakII.py
+++ 140-WordBreakII/140-WordBreakII.py
@@ -1,13 +1,19 @@
-# Last updated: 3/26/2025, 11:19:38 PM
+# Last updated: 3/26/2025, 11:32:32 PM
 class Solution:
-    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        n = len(s)
-        dp = [False] *(n+1)
-        dp[0] = True
-        for i in range(n):
-            for j in range(i,n):
-                if s[i:j+1] in wordDict and dp[i]:
-                    dp[j+1] = True
-                    
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        self.res = []    
+        self.dfs(0,s,wordDict,[])
+
+        return self.res
+
+    def dfs(self, index, s,wordDict,arr):
+        if index == len(s):
+            self.res.append(" ".join(arr))
+            return 
         
-        return dp[-1]
+        for i in range(index,len(s)):
+            if s[index:i+1] in wordDict:
+                arr.append(s[index:i+1])
+                self.dfs(i+1, s,wordDict,arr)
+                arr.pop()
+    
