089b9a0b39da4b536daeea6c28fa21ae637870ae
thehanemperor <sh442555789@gmail.com>
2025-03-30 02:09:12 -0700

    File updated at 3/30/2025, 2:09:13 AM

--- 299-BullsandCows/299-BullsandCows.py
+++ 299-BullsandCows/299-BullsandCows.py
@@ -1,20 +1,19 @@
-# Last updated: 3/30/2025, 2:01:42 AM
+# Last updated: 3/30/2025, 2:09:13 AM
 class Solution:
     def getHint(self, secret: str, guess: str) -> str:
-        seen = {}
-        for i in range(len(secret)):
-            seen[secret[i]] = seen.get(secret[i],0) + 1
+        secretMap = {}
+        for char in secret:
+            secretMap[char] = secretMap.get(char,0) + 1
 
-        bull = 0
-        cow = 0
+        black = 0
+        white = 0
         for i in range(len(guess)):
             if guess[i] == secret[i]:
-                bull += 1
+                black += 1
 
         for i in range(len(guess)):
+            if guess[i] in secretMap and secretMap[guess[i]] >0:
+                white += 1
+                secretMap[guess[i]] -= 1
 
-            if guess[i] in seen and seen[guess[i]] >0:
-                cow += 1
-                seen[guess[i]] -= 1
-
-        return "{}A{}B".format(bull,cow-bull)
+        return "{}A{}B".format(black, white - black)
