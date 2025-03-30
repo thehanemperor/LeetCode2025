6ab571c945dd99325564a2cf4b5dae4f2c98e1d4
thehanemperor <sh442555789@gmail.com>
2025-03-30 01:20:24 -0700

    File updated at 3/30/2025, 1:20:25 AM

--- 1383-MaximumPerformanceofaTeam/1383-MaximumPerformanceofaTeam.py
+++ 1383-MaximumPerformanceofaTeam/1383-MaximumPerformanceofaTeam.py
@@ -1,17 +1,22 @@
+# Last updated: 3/30/2025, 1:20:25 AM
 class Solution:
     def maxPerformance(self, n: int, speed: List[int], efficiency: List[int], k: int) -> int:
-        pairs = [(efficiency[i],speed[i]) for i in range(n)]
-        pairs.sort(key = lambda x:-x[0])
-        heap = []
+        # performance = sum(speeds) * min(efficiency)
+        # go from high efficiency and maintain a high speed as well
+        workers = [(efficiency[i],speed[i]) for i in range(n)]
+        workers.sort(key= lambda x:-x[0])
+        
+        speedHeap = []
+        res = 0 
         speedSum = 0
-        res = 0
-
-        for eff, currSpeed in pairs:
-            if len(heap) >= k:
-                speedSum -= heapq.heappop(heap)
-            heapq.heappush(heap,currSpeed)
+        for currEff, currSpeed in workers:
+            # efficiency is decreasing, everytime we get a lower eff
+            if len(speedHeap) >= k:
+                lowspeed = heappop(speedHeap)
+                speedSum -= lowspeed
+            heappush(speedHeap, currSpeed)
             speedSum += currSpeed
+            res = max(res, speedSum * currEff)
 
-            res = max(res,speedSum * eff)
-
-        return res % (10**9 + 7)
+        return res % (10**9 +7)
+            
