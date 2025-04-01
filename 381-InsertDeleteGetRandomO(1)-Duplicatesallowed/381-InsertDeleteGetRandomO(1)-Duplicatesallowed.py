4ef967a8610cb632261e73fe5822c2257e187cff
thehanemperor <sh442555789@gmail.com>
2025-04-01 16:13:35 -0700

    File updated at 4/1/2025, 4:13:35 PM

--- 381-InsertDeleteGetRandomO(1)-Duplicatesallowed/381-InsertDeleteGetRandomO(1)-Duplicatesallowed.py
+++ 381-InsertDeleteGetRandomO(1)-Duplicatesallowed/381-InsertDeleteGetRandomO(1)-Duplicatesallowed.py
@@ -1,38 +1,33 @@
-# Last updated: 4/1/2025, 2:50:28 PM
-class RandomizedSet:
+# Last updated: 4/1/2025, 4:13:35 PM
+class RandomizedCollection:
 
     def __init__(self):
         self.stack = []
-        self.stackIndex= {}
+        self.stackIndex = defaultdict(set)
 
     def insert(self, val: int) -> bool:
-        if val in self.stackIndex:
-            return False
-        self.stack.append(val)
-        self.stackIndex[val] = len(self.stack)-1
-        return True
+            self.stack.append(val)
+            self.stackIndex[val].add(len(self.stack)-1)
+            return len(self.stackIndex[val]) == 1 # because of empty set
+        
 
     def remove(self, val: int) -> bool:
-        if val not in self.stackIndex:
+        if val not in self.stackIndex or not self.stackIndex[val]:
             return False
-        index = self.stackIndex[val]
-        last = len(self.stack)-1
-        switch = self.stack[last]
-        self.stack[index],self.stack[last] = self.stack[last],self.stack[index]
-        
-        self.stackIndex[switch] = index
+        last = self.stack[-1]
+        valIndex = self.stackIndex[val].pop()
+        self.stack[valIndex] = last
+        self.stackIndex[last].add(valIndex)
+        self.stackIndex[last].remove(len(self.stack)-1) #only remove one element
         self.stack.pop()
-        self.stackIndex.pop(val)
         return True
-        
 
     def getRandom(self) -> int:
         return random.choice(self.stack)
-        
 
 
-# Your RandomizedSet object will be instantiated and called as such:
-# obj = RandomizedSet()
+# Your RandomizedCollection object will be instantiated and called as such:
+# obj = RandomizedCollection()
 # param_1 = obj.insert(val)
 # param_2 = obj.remove(val)
 # param_3 = obj.getRandom()
