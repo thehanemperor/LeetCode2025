e8cbfb242ecd7a1e251d240d6a686bd7e9fa1bd7
thehanemperor <sh442555789@gmail.com>
2025-04-07 01:19:37 -0700

    File updated at 4/7/2025, 1:19:38 AM

--- 3076-ShortestUncommonSubstringinanArray/3076-ShortestUncommonSubstringinanArray.py
+++ 3076-ShortestUncommonSubstringinanArray/3076-ShortestUncommonSubstringinanArray.py
@@ -1,37 +1,21 @@
-# Last updated: 4/7/2025, 1:03:43 AM
+# Last updated: 4/7/2025, 1:19:38 AM
 class Solution:
     def shortestSubstrings(self, arr: List[str]) -> List[str]:
-        substringMap = defaultdict(list)
-        for string in arr:
-            unique = set()
-            for start in range(len(string)):
-                for end in range(start,len(string)):
-                    tmp = string[start:end+1]
-                    unique.add(tmp)
-            for substring in unique:
-                substringMap[substring].append(string)
-
-        resMap = defaultdict(list)
-        for string in arr:
-            for substring, stringList in substringMap.items():
-                if len(stringList) == 1 and stringList[0] == string:
-                    if not resMap[string]:
-                        resMap[string].append(substring)
-                    else:
-                        if len(substring) <= len(resMap[string][0]):
-                            if len(substring) < len(resMap[string][0]):
-                                resMap[string].pop()
-                                resMap[string].append(substring)
-                            else:
-                                if substring < resMap[string][0]:
-                                    resMap[string].pop()
-                                    resMap[string].append(substring)
-
+        sub_strings = collections.defaultdict(set)
+        for idx, s in enumerate(arr):
+            n = len(s)
+            for i in range(n):
+                for j in range(i + 1, n + 1):
+                    sub_strings[s[i : j]].add(idx)
+        
         res = []
-        for string in arr:
-            if not resMap[string]:
-                res.append("")
-            else:
-                res.append(resMap[string][0])
-
+        for idx, s in enumerate(arr):
+            n = len(s)
+            match = []
+            for i in range(n):
+                for j in range(i + 1, n + 1):
+                    if len(sub_strings[s[i : j]]) == 1: match.append(s[i : j])
+            match.sort(key=lambda x : (len(x), x))
+            if not match: res.append("")
+            else: res.append(match[0])
         return res
